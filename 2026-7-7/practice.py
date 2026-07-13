97f08e4c212d8a3b4275328be440a5072e45c5bd
eunjae9988 <eunjae9988@gmail.com>
2026-07-13 12:53:48 -0700

    Update practice.py

--- 2026-7-7/practice.py
+++ 2026-7-7/practice.py
@@ -1,1 +1,8 @@
-
+#creating variables and assigning values
+number=0
+roll_width=1.4 
+price_per_metre=5
+filenmae='data.txt'
+trace=False
+sentence="this is a whole lot of nothing"
+total_price=roll_width * price_per_metre
