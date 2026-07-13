1953007969566f61ce025a09fb762c0ccfe48221
eunjae9988 <eunjae9988@gmail.com>
2026-07-13 12:55:33 -0700

    Update practice.py

--- 2026-7-7/practice.py
+++ 2026-7-7/practice.py
@@ -6,3 +6,13 @@ filenmae='data.txt'
 trace=False
 sentence="this is a whole lot of nothing"
 total_price=roll_width * price_per_metre
+
+#Joing up Text Fragments
+>>Race="Elf"
+>>Gender="Female"
+>>print(Gender,Race)
+#output is Female Elf
+
+#if 
+>>print("Male" "Gnome")
+#output is MaleGnome
